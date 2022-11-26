501642e72710b2f1e520b3ee4ef80960749f55c0
Jack Shrader <96661942+jpshrader@users.noreply.github.com>
2022-11-26 00:50:14 -0500

    refactor

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -30,3 +30,14 @@ public class WeatherForecastController : ControllerBase
         .ToArray();
     }
 }
+
+private class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
